ccd05a04d5f3cd0e06401ee42bfc641afa2850a2
mdrasb <muhammadrasekb@gmail.com>
2026-03-21 23:08:38 +0600

    update: updating main.go while plottin health.go, ingest.go, routes.go, server.go and creating a new file handler/utils.go

--- internal/handler/ingest.go
+++ internal/handler/ingest.go
@@ -1,1 +1,25 @@
 package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type IngestResponse struct {
+	Message string `json:"message"`
+}
+
+func HandleIngest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		writeJSON(w, http.StatusMethodNotAllowed, map[string]string{
+			"error": "method not allowed",
+		})
+		return
+	}
+
+	writeJSON(w, http.StatusCreated, IngestResponse{
+		Message: "received",
+	})
+
+	fmt.Println("Received request:", r.Method)
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,27 @@
 package server
+
+import (
+	"net/http"
+)
+
+type Server struct {
+	addr string
+	mux  *http.ServeMux
+}
+
+func NewServer(addr string) *Server {
+	mux := http.NewServeMux()
+
+	s := &Server{
+		addr: addr,
+		mux:  mux,
+	}
+	
+	s.registerRoutes()
+
+	return s
+}
+
+func (s *Server) Start() error {
+	return http.ListenAndServe(s.addr, s.mux)
+}
