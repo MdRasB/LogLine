0c7db9e1daa01534d3d2f434bb36170e7ea84216
mdrasb <muhammadrasekb@gmail.com>
2026-05-31 16:31:52 +0600

    auth (password): added password to pass_hash generator logic

--- internal/auth/password.go
+++ internal/auth/password.go
@@ -1,1 +1,40 @@
 package auth
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	BcryptCost = 12
+	MinPasswordLength = 8
+)
+
+func HashPassword(password string) (string, error) {
+	if password == "" {
+		return "", errors.New("password can't be empty")
+	}
+
+	if len(password) < 8 {
+		return "", errors.New("password must be 8 character long")
+	}
+
+	hash , err := bcrypt.GenerateFromPassword(
+		[]byte(password),
+		BcryptCost,
+	)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
+
+func VerifyPassword (password, hash string) error {
+	return bcrypt.CompareHashAndPassword(
+		[]byte(hash),
+		[]byte(password),
+	)
+}
+
