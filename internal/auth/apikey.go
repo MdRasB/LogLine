d04123732dac4f70ebb0f269958a1a153ee5b4ee
mdrasb <muhammadrasekb@gmail.com>
2026-05-31 17:10:14 +0600

    auth (api): adding logic for generating api, api to sha256, comparing api

--- internal/auth/apikey.go
+++ internal/auth/apikey.go
@@ -1,1 +1,41 @@
 package auth
+
+import (
+	//"crypto"
+	"crypto/rand"
+	"crypto/sha256"
+	"crypto/subtle"
+	"encoding/hex"
+	//"errors"
+	"fmt"
+)
+
+const (
+	APIKeyPrefix = "ll_live_"
+	TestAppend = "ll_live_"
+)
+
+func GenerateAPIKey()(string, error) {
+	key := make([]byte, 32)
+
+	if _, err := rand.Read(key); err != nil {
+		return "", fmt.Errorf("generate api key: %w\n", err)
+	}
+
+	return	APIKeyPrefix + hex.EncodeToString(key), nil
+}
+
+func HashAPIKey(s string) (string){
+	hash := sha256.Sum256([]byte(s))
+
+	return hex.EncodeToString(hash[:])
+}
+
+func VerifyAPIKey(key, storedHash string) bool {
+	ComputedHash := HashAPIKey(key)
+
+	return subtle.ConstantTimeCompare(
+		[]byte(ComputedHash),
+		[]byte(storedHash),
+	) == 1
+}
