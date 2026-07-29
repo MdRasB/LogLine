63767a3d2b1940e1162407d4a4d41ebc48585761
Rasek <muhammadrasekb@gmail.com>
2026-07-29 21:10:33 +0600

    mod(dashboard): adding the sql chart (logs), and some modification to dashboard page

--- internal/db/dashboard_store.go
+++ internal/db/dashboard_store.go
@@ -6,14 +6,8 @@ import (
 	"github.com/MdRasB/LogLine/internal/dashboard"
 )
 
-func (s *DBStore) GetDashboardStats(
-	ctx context.Context,
-) (*dashboard.DashboardStats, error) {
+func (s *DBStore) GetDashboardStats(ctx context.Context) (*dashboard.DashboardStats, error) {
 	stats := &dashboard.DashboardStats{}
-
-	// ----------------------------------------
-	// Total Logs
-	// ----------------------------------------
 
 	err := s.db.QueryRow(
 		ctx,
@@ -26,10 +20,6 @@ func (s *DBStore) GetDashboardStats(
 		return nil, err
 	}
 
-	// ----------------------------------------
-	// Total Services
-	// ----------------------------------------
-
 	err = s.db.QueryRow(
 		ctx,
 		`
@@ -41,10 +31,6 @@ func (s *DBStore) GetDashboardStats(
 		return nil, err
 	}
 
-	// ----------------------------------------
-	// Error Logs
-	// ----------------------------------------
-
 	err = s.db.QueryRow(
 		ctx,
 		`
@@ -55,6 +41,80 @@ func (s *DBStore) GetDashboardStats(
 	).Scan(&stats.ErrorLogs)
 	if err != nil {
 		return nil, err
+	}
+
+	rows, err := s.db.Query(
+		ctx,
+		`
+		SELECT TO_CHAR(DATE_TRUNC('hour', timestamp),'YYYY-MM-DD HH24:00') AS hour, COUNT(*)
+		FROM logs
+		GROUP BY DATE_TRUNC('hour', timestamp)
+		ORDER BY DATE_TRUNC('hour', timestamp);
+		`,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var h dashboard.HourlyStat
+
+		err := rows.Scan(&h.Hour, &h.Count)
+		if err != nil {
+			return nil, err
+		}
+
+		stats.Volume = append(stats.Volume, h)
+	}
+
+	rows, err = s.db.Query(
+		ctx,
+		`
+		SELECT level, COUNT(*) FROM logs
+		GROUP BY level
+		ORDER BY level;
+		`,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var l dashboard.LevelStat
+
+		err := rows.Scan(&l.Level, &l.Count)
+		if err != nil {
+			return nil, err
+		}
+
+		stats.Levels = append(stats.Levels, l)
+	}
+
+	rows, err = s.db.Query(
+		ctx,
+		`
+		SELECT service, COUNT(*) FROM logs
+		GROUP BY service
+		ORDER BY service;
+		`,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var s dashboard.ServiceStat
+
+		err := rows.Scan(&s.Service, &s.Count)
+		if err != nil {
+			return nil, err
+		}
+
+		stats.Services = append(stats.Services, s)
 	}
 
 	return stats, nil
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,1 +1,131 @@
-console.log("LogLine Static JS Loaded");
+document.addEventListener("DOMContentLoaded", () => {
+
+    const dataElement = document.getElementById("dashboard-data");
+
+    if (!dataElement) {
+        return;
+    }
+
+    const dashboard = JSON.parse(dataElement.textContent);
+
+    createVolumeChart(dashboard.volume);
+
+    createLevelChart(dashboard.levels);
+
+    createServiceChart(dashboard.services);
+
+});
+
+function createVolumeChart(data) {
+
+    const canvas = document.getElementById("volumeChart");
+
+    if (!canvas) return;
+
+    new Chart(canvas, {
+        type: "line",
+
+        data: {
+
+            labels: data.map(v => v.hour),
+
+            datasets: [{
+                label: "Logs",
+
+                data: data.map(v => v.count),
+
+                borderWidth: 2,
+
+                tension: 0.3,
+
+                fill: false
+
+            }]
+        },
+
+        options: {
+
+            responsive: true,
+
+            maintainAspectRatio: true,
+
+            aspectRatio: 2,
+
+        }
+    });
+
+}
+
+function createLevelChart(data) {
+
+    const canvas = document.getElementById("levelChart");
+
+    if (!canvas) return;
+
+    new Chart(canvas, {
+
+        type: "pie",
+
+        data: {
+
+            labels: data.map(v => v.level),
+
+            datasets: [{
+
+                data: data.map(v => v.count)
+
+            }]
+
+        },
+
+        options: {
+
+            responsive: true,
+
+            maintainAspectRatio: true,
+
+            aspectRatio: 1,
+
+        }
+
+    });
+
+}
+
+function createServiceChart(data) {
+
+    const canvas = document.getElementById("serviceChart");
+
+    if (!canvas) return;
+
+    new Chart(canvas, {
+
+        type: "bar",
+
+        data: {
+
+            labels: data.map(v => v.service),
+
+            datasets: [{
+
+                label: "Logs",
+
+                data: data.map(v => v.count)
+
+            }]
+
+        },
+
+        options: {
+
+            responsive: true,
+
+            maintainAspectRatio: true,
+
+            aspectRatio: 2,
+
+        }
+
+    });
+
+}
